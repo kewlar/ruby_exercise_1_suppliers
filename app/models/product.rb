4186c9fa7697c4f8b95d8649f5184593cc737603
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2017-03-28 19:38:23 +0300

    Added `#to_s` methods to supplier and product, for more human representations.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :supplier
   validates :ean, presence: true, format: { with: /\A[0-9]{8,14}\z/, message: "must be 8-14 characters, digits only. " }
+
+  def to_s
+    ean
+  end
 end
