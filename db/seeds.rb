57af7eaeac53079c3b0014ea935d70227b2c27cd
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2017-03-28 23:43:01 +0300

    Added seed.rb (because db:fixtures:load doesn't work on Heroku so simply).

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+tyler_durden = Supplier.create(
+    name: "Tyler Durden",
+    address: "420, Paper St. Wilmington, DE 19886"
+)
+Product.create(
+    ean: "0011111614246",
+    description: "You are not special. You're not a beautiful and unique snowflake. You're the same decaying organic matter as everything else. We're all part of the same compost heap. We're all singing, all dancing crap of the world.",
+    supplier: tyler_durden
+)
+
+fred_flintstone = Supplier.create(
+    name: "Fred Flintstone",
+    address: "301 Cobblestone Way, Bedrock 70777"
+)
+stone = Product.create(
+    ean: "0045635919259",
+    description: "Rocks",
+    supplier: fred_flintstone
+)
+
+weasleys = Supplier.create(
+    name: "Weasleys' Wizard Wheezes",
+    address: "93 Diagon Alley, London, England"
+)
+Product.create(
+  ean: "0721773570032",
+  description: "Anti Gravity Hat",
+  supplier: weasleys
+)
+Product.create(
+  ean: "0837303482550",
+  description: "Boxing Telescope",
+  supplier: weasleys
+)
