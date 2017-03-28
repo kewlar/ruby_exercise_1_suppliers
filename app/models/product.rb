cbce3074578787650407ea8b9d3e1d0d8f358583
Mindaugas Pelionis <mindaugas.pelionis@gmail.com>
2017-03-28 19:00:56 +0300

    Added validation: product EAN must be 8-14 characters, digits only.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,4 @@
 class Product < ApplicationRecord
   belongs_to :supplier
+  validates :ean, presence: true, format: { with: /\A[0-9]{8,14}\z/, message: "must be 8-14 characters, digits only. " }
 end
